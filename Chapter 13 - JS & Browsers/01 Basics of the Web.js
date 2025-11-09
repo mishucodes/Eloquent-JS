b8ab505ef4f2c11344781a7f288a9495f5b63999
Manish Goyal <manishgoyal5683@gmail.com>
2025-11-09 19:29:21 +0530

    chapter 13

---
+++ Chapter 13 - JS & Browsers/01 Basics of the Web.js
@@ -0,0 +1,14 @@
+//01. Internet: Basically two/more computers sharing information with one-another. Today, it means almost all computers on Earth being able to communicate with one-another.
+
+//02. World-Wide-Web: A set of protocols & formats to allow us to visit web-pages through a web-browser. Example of a "website":
+    //https://worldofmishu.in/index.html
+        //a) https:// -> protocol (how to get)
+        //b) worldofmishu.in -> server (where to get)
+        //c) /index.html -> path to resource (what to get)
+
+//03. HTML: It's a format of a document. These documents are called "web-pages". They can link to each-other.
+
+//04. Sandboxing: Browsers strictly allow only certain permissions to all websites. For example, they can't really access files on your system, etc. It's quite surprising that governments have not yet demanded special treatment for their websites.
+
+
+//NOTE: I won't go into much details about technologies like HTML or HTTP in these notes. Perhaps some day when I study some book on HTTP, etc. Let's just focus on JS in this repo.
